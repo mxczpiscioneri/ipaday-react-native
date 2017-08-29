3db30e0bf5261a2b925dfadd12953ee0ae1a1f07
Matheus Piscioneri <mxczpiscioneri@gmail.com>
2017-08-28 23:31:06 -0300

    Create rating beer

--- src/container/beers/actions.js
+++ src/container/beers/actions.js
@@ -1,6 +1,7 @@
-import { ToastAndroid } from 'react-native';
+import { ToastAndroid, AsyncStorage } from 'react-native';
 
 import api from '../../utils/api';
+import firebase from '../../utils/firebase';
 import { FIND_BEERS, GET_BEER, SHOW_LOADING } from '../../types';
 
 export const allBeers = () => {
@@ -32,5 +33,31 @@ export const getBeer = (beerId) => {
         ToastAndroid.show('Erro ao carregar cerveja. ', ToastAndroid.LONG);
         console.log("Erro getBeer: ", error);
       });
+  }
+}
+
+export const setRating = (beerId, rating, comment) => {
+  return async dispatch => {
+      let user = await AsyncStorage.getItem('user');
+      user = JSON.parse(user);
+      user = user || {};
+
+      let listRatings = [];
+      firebase.database()
+        .ref(user.uid)
+        .once('value', (snap) => {
+          listRatings = snap.val();
+          listRatings = listRatings || {};
+          listRatings = listRatings.ratings || [];
+          listRatings.push({beerId, rating, comment});
+
+          firebase.database()
+            .ref(user.uid)
+            .set({
+              ratings: listRatings
+            });
+
+          ToastAndroid.show("Avaliação realizada com suceso!", ToastAndroid.LONG)
+        });
   }
 }
--- src/container/beers/detail.js
+++ src/container/beers/detail.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Container, Header, Left, Right, Body, Button, Icon, Title, Content, Text, Card, CardItem, Spinner } from 'native-base';
+import { StyleSheet, Image, Modal, View } from 'react-native';
+import { Container, Header, Left, Right, Body, Button, Icon, Title, Content, Text, Badge, Spinner, Form, Item, Label, Input } from 'native-base';
 import { connect } from 'react-redux';
 import Config from 'react-native-config';
+import StarRating from 'react-native-star-rating';
 
-import { getBeer } from './actions'
+import { getBeer, setRating } from './actions'
 
 class Beers extends Component {
   constructor(props) {
     super(props);
-  }
 
-  componentWillMount() {
-    const { params } = this.props.navigation.state;
-    let beerId = params || {};
-    beerId = beerId.beerId || '';
-    this.props.getBeer(beerId);
+    this.state = {
+      rating: 0,
+      modalVisible: false,
+      comment: ''
+    };
   }
+  
+    componentWillMount() {
+      const { params } = this.props.navigation.state;
+      let beerId = params || {};
+      beerId = beerId.beerId;
+      this.props.getBeer(beerId);
+    }
+    
+    _send() {
+      const { params } = this.props.navigation.state;
+      let beerId = params || {};
+      beerId = beerId.beerId || '';
+      this.props.setRating(beerId, this.state.rating, this.state.comment)
+
+      this.setState({ modalVisible: false });
+    }
+    
+    _setRating(rating) {
+      this.setState({ rating: rating });
+      this.setState({ modalVisible: true })
+    }
+
+    _closeModal() {
+      this.setState({ modalVisible: false })
+    }
 
   render() {
     return (
@@ -35,38 +60,115 @@ class Beers extends Component {
           {this.props.loading ? (
             <Spinner />
           ) : (
-            <Card>
-              <CardItem cardBody style={{ alignSelf: 'center', marginTop: 16 }}>
+            <View style={{ paddingHorizontal: 16 }}>
+              <View style={{ alignSelf: 'center', marginTop: 16 }}>
                 <Image source={{uri: `${Config.URL_S3}/${this.props.beer.image}`}} style={{height: 250, width: 250}}/>
-              </CardItem>
-              <CardItem style={{ marginTop: 8 }}>
-                <Body>
-                  <Text>{this.props.beer.name}</Text>
-                  <Text>{this.props.beer.style}</Text>
-                  <Text>{this.props.beer.brewery} ({this.props.beer.city})</Text>
-                  <Text>{this.props.beer.details}</Text>
-                </Body>
-              </CardItem>
-              <CardItem style={{ marginTop: 8 }}>
-                <Left>
-                  <Text>ABV:</Text>
-                  <Text>{this.props.beer.ABV}</Text>
-                </Left>
-                <Body>
-                  <Text>IBU:</Text>
-                  <Text>{this.props.beer.IBU}</Text>
-                </Body>
-                <Right>
-                  <Text>{this.props.beer.year}</Text>
-                </Right>
-              </CardItem>
-            </Card>
-          )}
+              </View>
+              <Badge style={{backgroundColor: "#fdbe17"}}>
+                <Text>{this.props.beer.year}</Text>
+              </Badge>
+              <View style={{ marginTop: 8 }}>
+                <View style={{ alignSelf: 'center' }}>
+                  <Text style={styles.title}>{this.props.beer.name}</Text>
+                  <Text style={styles.text}>{this.props.beer.style}</Text>
+                  <Text style={styles.text}>{this.props.beer.brewery} ({this.props.beer.city})</Text>
+                </View>
+                <Text style={styles.text}>{this.props.beer.details}</Text>
+              </View>
+              <View style={{ marginTop: 24, flexDirection: 'row' }}>
+              <View style={{ flex: 1, alignItems: "center" }}>
+                  <Text style={styles.details}>ABV: {this.props.beer.ABV}</Text>
+                </View>
+                <View style={{ flex: 1, alignItems: "center" }}>
+                  <Text style={styles.details}>IBU: {this.props.beer.IBU}</Text>
+                </View>
+              </View>
+              <View style={{ marginTop: 24, flexDirection: 'row' }}>
+                <Text style={[ styles.text, {flex: 1, marginTop: 8}]}>Avalie essa cerveja!</Text>
+                <StarRating
+                  emptyStar={'ios-beer-outline'}
+                  fullStar={'ios-beer'}
+                  halfStar={'ios-beer'}
+                  iconSet={'Ionicons'}
+                  starStyle={{marginHorizontal: 5, flex: 1}}
+                  maxStars={5}
+                  rating={this.state.rating}
+                  selectedStar={(rating) => this._setRating(rating)}
+                  starColor={'#f94840'}
+                />
+              </View>
+            </View>            
+          )}          
         </Content>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          onRequestClose={() => false}
+          visible={this.state.modalVisible}>
+          <View style={styles.modal}>
+            <View style={styles.modalBody}>
+              <Text style={styles.modalTitle}>Avalie essa cerveja!</Text>
+              <Form>
+                <Item>
+                  <Input placeholder="Comentário..." onChangeText={(text) => this.setState({ comment: text })} />
+                </Item>
+              </Form>
+              <View style={{ flexDirection: 'row', marginTop: 20 }}>
+                <View style={{ flex: 1, paddingRight: 5 }}>
+                  <Button block transparent dark onPress={() => this._closeModal()}>
+                    <Text>Cancelar</Text>
+                  </Button>
+                </View>
+                <View style={{ flex: 1, paddingLeft: 5 }}>
+                  <Button block transparent dark onPress={() => this._send()}>
+                    <Text>Enviar</Text>
+                  </Button>
+                </View>
+              </View>
+            </View>
+          </View>
+        </Modal>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  modal: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
+    flex: 1
+  },
+  modalBody: {
+    backgroundColor: "#FFF",
+    padding: 20,
+    paddingBottom: 0
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  title: {
+    fontSize: 24,
+    color: "#404040",
+    textAlign: "center"
+  },
+  text: {
+    fontSize: 18,
+    color: "#404040",
+    textAlign: "center"
+  },
+  details: {
+    color: "#404040"
+  },
+  center: {
+    textAlign: "center"
+  }
+});
 
 const mapStateToProps = state => {
   return (
@@ -77,4 +179,4 @@ const mapStateToProps = state => {
   )
 }
 
-export default connect(mapStateToProps, { getBeer })(Beers)
+export default connect(mapStateToProps, { getBeer, setRating })(Beers)
